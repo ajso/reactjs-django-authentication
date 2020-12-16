106a34eabfde26e78d932e016f97b02428b46d40
unknown <AJSO>
2020-12-16 10:16:57 +0400

    production auth commit

---
+++ Backend/build/precache-manifest.45b9ef1702bef213685ded1edbfe8235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70080a7bb2500664a80a657d208455e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b05a1d59b5e66487126",
+    "url": "/static/css/main.39d66635.chunk.css"
+  },
+  {
+    "revision": "43ba976e36bd05019ce1",
+    "url": "/static/js/2.6c4616a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c4616a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b05a1d59b5e66487126",
+    "url": "/static/js/main.600a3346.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
